0bca72d21b170c38d2433131c6324b077af9e3c4
adon <86583577+shg166@users.noreply.github.com>
2023-06-18 09:12:07 +0900

    Add Code & Name List

---
+++ Mod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+using ColossalFramework.UI;
+using ICities;
+
+namespace JP_CampusNameList
+{
+	public class JP_CampusNameList : IUserMod
+	{
+
+		public string Name
+		{
+			get { return "JP University Name List (CNL) v1.0"; }
+		}
+
+		public string Description
+		{
+			get { return "Custom Name Listsを使って、DLC Campusのキャンパスエリア名を変更します"; }
+		}
+
+	}
+}
